6484c3622d008f3094af66cd21687fdc4529f1ae
Vyacheslav Voronenko <git@voronenko.info>
2017-03-16 19:35:24 +0200

    feature(packaging) base packaging logic

---
+++ package.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+HOME=$PWD
+PROJECT=project
+VERSIONFILE=_buildinfo.txt
+SKIPLIST="--exclude=.git --exclude=*.sh  --exclude=*.tgz"
+
+if [ -z "$1" ]
+then
+  SUFFIX=""
+else
+  SUFFIX="-$1"
+fi
+
+rm $PROJECT-*.tgz || true
+
+mkdir -p build
+
+./clean_build.sh
+
+VERSION=`cat version.txt`
+GITCOMMIT=`git rev-parse --short HEAD`
+GITBRANCH=`git rev-parse --abbrev-ref HEAD`
+DATE=`date +%Y-%m-%d:%H:%M:%S`
+
+
+echo "major_version=$VERSION" > $VERSIONFILE
+echo "minor_version=$1" >> $VERSIONFILE
+echo "git_hash=$GITCOMMIT" >> $VERSIONFILE
+echo "git_branch" >> $VERSIONFILE
+echo "built=$DATE" >> $VERSIONFILE
+echo "ARTIFACT_VERSION=$VERSION" >> $VERSIONFILE
+
+cp $VERSIONFILE ./build || true
+
+cd ./build
+
+echo tar cfz $PROJECT-$VERSION$SUFFIX.tgz . $SKIPLIST
+tar cfz $HOME/$PROJECT-$VERSION$SUFFIX.tgz . $SKIPLIST || true
